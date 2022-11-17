a55020996aed7bf1a53020ed39def9c268ee8d3e
design4greengom <118222385+design4greengom@users.noreply.github.com>
2022-11-17 22:51:57 +0100

    correctiong json file

--- parsing/main.py
+++ parsing/main.py
@@ -1,20 +1,23 @@
 import json
 
-def write_data(dict):
+
+def write_data(ListDic):
     with open('rapport.json', "a+", encoding='utf8') as report:
-        report.write(json.dumps(dict, indent=4, ensure_ascii=False))
+        report.write(json.dumps(ListDic, indent=4, ensure_ascii=False))
+
 
 def processline(row):
     listElement = row.split(';')
     isEmpty = 0
     for element in listElement:
-        if not element or isEmpty == 5 :
+        if not element or isEmpty == 5:
             isEmpty += 1
     if isEmpty <= 5:
         ListFormation.append(row)
 
-ListFormation=[]
-#clean/mise en forme CSV
+
+ListFormation = []
+# clean/mise en forme CSV
 with open('data_fr.csv', 'r', encoding='utf8') as file:
     creatline = ''
     for row in file:
@@ -23,22 +26,26 @@ with open('data_fr.csv', 'r', encoding='utf8') as file:
         if row.count(';') == 14:
             processline(row)
         elif not creatline:
-                creatline += row.rstrip()
+            creatline += row.rstrip()
         else:
             creatline += row.rstrip()
             if creatline.count(';') == 14:
                 processline(creatline)
                 creatline = ''
 
-#Création json
-listTitle =  ListFormation[0].split(';')
+# Création json
+listTitle = ['id', 'nom formation', 'type', 'établissement', 'unité', 'cycle', 'Objectif', 'description', 'organisme', \
+             'intitulé', 'localisation', 'validation aquis', 'durée', 'accès', 'site']
+ListDic = []
 idxId = 0
 for element in ListFormation[1:]:
     jsonElement = {}
-    listelement =  element.split(';')
+    listelement = element.split(';')
     for idx, col in enumerate(listTitle):
-        if idx == 1 : 
+        if idx == 1:
             idxId += 1
-            jsonElement['id'] = idxId
+            jsonElement['id'] = str(idxId)
         jsonElement[col] = listelement[idx]
-    write_data(jsonElement)
+        jsonElement[col] = listelement[idx]
+        ListDic.append(jsonElement)
+write_data(ListDic)
